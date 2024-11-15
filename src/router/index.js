bad83e8f62581875156abcf2f9eea1e84857e4b6
Mrtza-javidi <morteza.javidi.webdev@gmail.comi>
2024-11-16 00:59:42 +0330

    feature(router): implement router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-  // { path: "/", redirect: "/login" },
-  // {
-  //   path: "/login",
-  //   name: "login",
-  //   component: () => import("@/views/login-page.vue"),
-  // },
+  { path: "/", redirect: "/todos" },
+  {
+    path: "/todos",
+    name: "todos",
+    component: () => import("../views/todos-list.vue"),
+  },
+  {
+    path: "/todos/:id",
+    name: "TodoDetails",
+    component: () => import("@/views/todo-details.vue"),
+    // props: true, // Pass route params as props
+  },
 ];
 
 const router = createRouter({
